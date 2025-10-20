2637ef8770131e87fed1d0ca99dc9848f8c04bf3
Ugnius Vaičeskas <ugnius@venderes.com>
2025-10-20 03:35:47 +0300

    Prideti autentifikacijos ir JWT servisai + prideti reikiami DI

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using Groovo.Filters;
 using Groovo.Data.Contexts;
 using Groovo.Data;
+using Groovo.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Groovo;
 
@@ -25,6 +27,13 @@ public class Program
             }
         });
 
+        // Register services
+        builder.Services.AddScoped<IJwtService, JwtService>();
+        builder.Services.AddScoped<IAuthService, AuthService>();
+        
+        // Add Identity hasher
+        builder.Services.AddScoped<IPasswordHasher<Models.User>, PasswordHasher<Models.User>>();
+
         builder.Services.AddControllers(options =>
         {
             options.Filters.Add<ApiResponseFilter>();
@@ -41,6 +50,7 @@ public class Program
         builder.Services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Groovo API", Version = "v1" });
+            c.SchemaFilter<EnumDescriptionSchemaFilter>();
         });
         builder.Services.AddVersionedApiExplorer(options =>
         {
